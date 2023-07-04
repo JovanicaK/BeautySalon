367439d8d28bc52306faf77f43059807104e4e46
Jovanica.K <jovanica0312@gmail.com>
2023-07-04 12:35:31 +0200

    Added files from old project

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+var $nav = $('.nav');
+var opened = 'opened';
+var $header = $('.header')
+
+
+
+function navigation() {
+  var $toggle = $('.toggle');
+
+
+  $(document).ready(function(){
+    $toggle.on('click', function(){
+      $nav.animate({
+          width: 'toggle'
+      });
+    });
+    $('.toggle-close').on('click',function(){
+      $nav.animate({
+          width: 'toggle'
+      });
+    })
+  });
+}
+
+
+
+
+function backToTop() {
+  var $btn = $('.backtotop-btn');
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 150) {
+        $btn.fadeIn(300);
+      } else {
+        $btn.fadeOut(300);
+      }
+    });
+    $btn.on('click', function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, '300');
+    });
+}
+
+navigation();
+backToTop();
+
